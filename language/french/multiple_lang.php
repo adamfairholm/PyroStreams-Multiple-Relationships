a8d5efccaa61ec029fd3e8a07df1dc721f22e2f3
Adam Fairholm <adam.fairholm@gmail.com>
2013-02-26 22:28:08 -0500

    Removing BOM on french translation.
    Fixes #6

--- language/french/multiple_lang.php
+++ language/french/multiple_lang.php
@@ -1,4 +1,4 @@
-﻿<?php defined('BASEPATH') or exit('No direct script access allowed');
+<?php defined('BASEPATH') or exit('No direct script access allowed');
 
 $lang['streams.multiple.name'] 			= "Relations multiples";
 $lang['streams.multiple.choose_stream'] = "Stream en relation";
