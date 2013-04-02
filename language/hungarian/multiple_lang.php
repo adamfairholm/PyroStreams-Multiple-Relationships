4cc58a4d174db8098f84315e5c962888c3002486
Varga Péter <varga.peter@binvision.hu>
2013-04-02 03:22:57 +0200

    new lang: hu

---
+++ language/hungarian/multiple_lang.php
@@ -0,0 +1,8 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['streams:multiple.name'] 			= 'Kapcsolat - több elem';
+$lang['streams:multiple.choose_stream'] = 'Kapcsolat adatfolyam';
+$lang['streams:multiple.choose_ui']     = 'Vezérlőelem';
+$lang['streams:multiple.drag_drop']     = 'Fogd és vidd';
+$lang['streams:multiple.multiselect']   = 'Többszörös kijelölés';
+$lang['streams:multiple.no_change']     = 'Az adatfolyam csak létrehozáskor módosítható.';
