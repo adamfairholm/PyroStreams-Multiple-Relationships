63c2489f4054ad7ca7749af872c2775cd2a67823
Tony G. Bolaño <tonybolanyo@gmail.com>
2014-09-18 13:25:54 +0200

    agregadas traducciones

--- language/spanish/multiple_lang.php
+++ language/spanish/multiple_lang.php
@@ -1,8 +1,10 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed');
 
-$lang['streams:multiple.name'] 			= 'Multiples Relaciones';
-$lang['streams:multiple.choose_stream'] = 'Stream de Relación';
-$lang['streams:multiple.choose_ui']     = 'Choose UI'; #translate
-$lang['streams:multiple.drag_drop']     = 'Drag/Drop'; #translate
-$lang['streams:multiple.multiselect']   = 'Multiselect'; #translate
-$lang['streams:multiple.no_change']     = 'You cannot change a multiple relationship field stream once it has been assigned.'; #translate
+$lang['streams:multiple.name'] 			= 'Relaciones múltiples';
+$lang['streams:multiple.choose_stream'] = 'Selecciona stream';
+$lang['streams:multiple.choose_ui']     = 'Selecciona interfaz';
+$lang['streams:multiple.drag_drop']     = 'Arrastar/Soltar';
+$lang['streams:multiple.multiselect']   = 'Multiselección';
+$lang['streams:multiple.no_change']     = 'No puedes cambiar un campo de relación múltiple después de haber sido asignado.';
+$lang['streams:multiple.available_opt'] = 'Opciones disponibles';
+$lang['streams:multiple.selected_opt']  = 'Opciones seleccionadas';
